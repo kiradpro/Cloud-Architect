96659a0b944b2713956b609f50a8fa3f1a7e1627
kiradpro <kiradpro@users.noreply.github.com>
2021-05-18 22:31:32 +0200

    v0.6

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from flask import abort, redirect, url_for, make_response, flash
-from azuredb import AzureDB
+from azuredatabase import AzureDB
 
 
 
