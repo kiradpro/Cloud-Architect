0f275e1a5047a2758be6c81d22aab933f79f0a28
kiradpro <kiradpro@users.noreply.github.com>
2021-05-18 23:44:57 +0200

    v0.5

--- azurecred.py
+++ azurecred.py
@@ -1,5 +1,5 @@
-AZDBSERVER = 'flaskserver10.database.windows.net'
+AZDBSERVER = 'tcp:flaskserver10.database.windows.net'
 AZDBNAME = 'Flaskapp' 
 AZDBUSER = 'adminf' 
 AZDBPW = 'Inf.00000001' 
-AZDBDRIVER = '{ODBC Driver 17 for SQL Server}'
+AZDBDRIVER = '{ODBC Driver 13 for SQL Server}'
